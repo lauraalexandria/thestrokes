168b6705ad8f0c40a358ef636291b757a3e1a788
lauraalexandria <57160675+lauraalexandria@users.noreply.github.com>
2020-11-23 22:57:01 -0300

    raspagem das letras

---
+++ WebScraping.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(httr)
+library(xml2)
+library(rvest)
+library(furrr)
+
+# Número de Músicas: ----
+
+read_html("https://www.vagalume.com.br/the-strokes/") %>% 
+  html_nodes(".nameMusic") %>% html_text() %>% 
+  unique() %>% length()
+
+# Obtendo todas as letras de músicas ----
+
+get_lyric <- function(song_link){
+  
+  # Reading the html page
+  lyric <- read_html(paste0("https://www.vagalume.com.br", song_link)) %>% html_nodes("#lyrics")
+  
+  # Creating sep to replace linebreaks with ". "
+  dummy <- xml_node(read_xml("<doc><span>. </span></doc>"), "span")
+  
+  # Replacing line-breaks
+  xml_add_sibling(xml_nodes(lyric, "br"), dummy)
+  
+  res <- lyric %>% html_text()
+  
+  return(res)
+}
+
+get_lyric("/the-strokes/you-only-live-once.html")
+
+get_lyrics_links <- function(artist_link){
+  
+  # Reading the html page on the artist
+  page <- read_html(paste0("https://www.vagalume.com.br", artist_link))
+  
+  # Obtaining all the musics' links -
+  music_name_node <- page %>% html_nodes(".nameMusic")
+  music_names <- music_name_node %>% html_text()
+  music_links <- music_name_node %>% html_attr("href")
+  
+  # Building final tibble
+  res <- tibble(ALink = rep(artist_link, length(music_names)),
+                SName = music_names,
+                SLink = music_links)
+  
+  return(res)
+}
+
+
+# Returning all the links to the musics
+plan(multisession)
+musicas <- get_lyrics_links("/the-strokes/")
+
+letras <- apply(musicas[,3], 1, get_lyric)
+letras <- letras[-(1:25)]
+
+# Fonte: https://aneisse.com/post/2019-02-10-music-data-scraping/2019-02-10-music-data-scraping/
