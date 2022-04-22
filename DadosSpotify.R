ba5acbfc920a5dd6b3b7220e29b28cd99b938d5a
lauraalexandria <57160675+lauraalexandria@users.noreply.github.com>
2022-04-22 15:20:09 -0300

    vindo dados spotify

--- DadosSpotify.R
+++ DadosSpotify.R
@@ -10,7 +10,15 @@ access_token <- get_spotify_access_token()
 
 # Bancos de Dados ---------------------------------------------------------------------
 
-teste <- get_artist_audio_features("The Strokes") %>% 
-  select(track_id, track_name, artist_id, artist_name, 
-         album_name, danceability, energy, loudness, speechiness, 
-         acousticness, instrumentalness, liveness, valence, tempo)
+dados_spotify <- get_artist_audio_features(
+  "The Strokes",
+  include_groups = c("album", "single") # Tanto as músicas de álbuns quanto de eps;
+  ) %>% 
+  select(track_name, album_name, danceability, energy, loudness, speechiness, 
+         acousticness, instrumentalness, liveness, valence, tempo) %>% 
+  distinct(track_name, .keep_all = T) %>% 
+  mutate(
+    track_name = str_remove_all(track_name, '"'), # Remover aspas dos títulos para evitar problemas
+    track_name = str_to_title(track_name),        # Padronizar os títulos;
+    track_name = str_replace(track_name, "&", "And")
+    ) 
--- WebScraping.R
+++ WebScraping.R
@@ -71,35 +71,5 @@ albums <- function (name, message = TRUE) {
   return(albums)
 }
 
-albuns <- data.frame(SName = c("Is This It", "The Modern Age", "Alone, Together", 
-                               "Barely Legal", "Someday", "Last Nite", "Hard To Explain",
-                               "When It Started", "Trying Your Luck", "Take It Or Leave It",
-                               "What Ever Happened?", "Reptilia", "Automatic Stop", 
-                               "12:51", "You Talk Way Too Much", "Between Love And Hate",
-                               "Meet Me In The Bathroom", "Under Control", "The Way It Is",
-                               "The End Has No End", "I Can't Win", "You Only Live Once",
-                               "Juicebox", "Heart In A Cage", "Razorblade", 
-                               "On The Other Side", "Vision Of Division", "Ask Me Anything",
-                               "Electricityscape", "Killing Lies", "Fear Of Sleep", 
-                               "15 Minutes", "Ize Of The World", "Evening Sun", "Red Light",
-                               "Machu Picchu", "Under Cover Of Darkness", "Two Kinds Of Happiness",
-                               "You're So Right", "Taken For a Fool", "Games", "Call Me Back", 
-                               "Gratisfaction", "Metabolism", "Life Is Simple In The Moonlight", 
-                               "Tap Out", "All The Time", "One Way Trigger", "Welcome To Japan",
-                               "80's Comedown Machine", "50/50", "Slow Animals", 
-                               "Partners In Crime", "Chances", "Happy Ending", 
-                               "Call It Fate, Call It Karma", "Drag Queen", "Oblivious", 
-                               "Threat of Joy", "The Adults Are Talking", "Selfless", 
-                               "Brooklyn Bridge To Chorus", "Bad Decisions", "Eternal Summer",
-                               "At The Door", "Why Are Sunday's So Depressing", 
-                               "Not The Same Anymore", "Ode To The Mets"),
-                     Album = c(rep("Is This It", 10),
-                               rep("Room On Fire", 11),
-                               rep("First Impressions of Earth", 14),
-                               rep("Angles", 10),
-                               rep("Comedown Machine", 11),
-                               rep("Future Present Past [EP]", 3),
-                               rep("The New Abnormal", 9)))
-
 # Fonte: https://aneisse.com/post/20190210-music-data-scraping/20190210-music-data-scraping/
 
